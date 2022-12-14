bea22fcb40ebfa5eee69880f111e7e8fbaa9dd6d
Thien Trieu <thien.trieu@telus.com>
2022-12-13 21:34:08 -0800

    Add LightBnB_WebApp folder

--- 1_queries/2_property_listing_by_city.sql
+++ 1_queries/2_property_listing_by_city.sql
@@ -1,6 +1,6 @@
 SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
 FROM properties
-JOIN property_reviews ON properties.id = property_id
+LEFT JOIN property_reviews ON properties.id = property_id
 WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
 HAVING avg(property_reviews.rating) >= 4
