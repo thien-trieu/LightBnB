1df93e6ada40f665daf614b1b3fa79f913feae0b
Thien Trieu <thien.trieu@telus.com>
2022-12-13 21:46:44 -0800

    install node-postres and connect to lightbnb database in server/database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
 /// Users
 
 /**
