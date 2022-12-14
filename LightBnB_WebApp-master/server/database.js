d8997e20b6d0911efd6e2c9d655fc50f364ba6cd
Thien Trieu <thien.trieu@telus.com>
2022-12-14 10:14:41 -0800

    refactor getUserWithID function for database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -55,7 +55,25 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+
+  const queryString = `
+  SELECT *
+  FROM users
+  WHERE id = $1`;
+  
+return pool
+  .query(queryString, [id])
+  .then((result) => {
+    // console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    // console.log(err.message);
+    return null
+  });
+
+
+  // return Promise.resolve(users[id]);
 }
 exports.getUserWithId = getUserWithId;
 
